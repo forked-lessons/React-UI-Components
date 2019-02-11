0aefb51c2c8200a0723ce9e55db6890b9ada216c
Daniel <danielstarling4@gmail.com>
2019-02-11 14:13:16 -0700

    components all linked together

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+
+
+
+function HeaderTitle() {
+    return (
+        <div>
+            <h1>Lambda School!</h1>
+            <h2>@LambdaSchool 26 Jan</h2>
+        </div>
+    );
+};
+
+export default HeaderTitle;
