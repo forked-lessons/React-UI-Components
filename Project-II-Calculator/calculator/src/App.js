2bf357240c6fa3b32b55d444c6b6bb9b21e63803
Daniel <danielstarling4@gmail.com>
2019-02-12 15:28:33 -0700

    MVP complete

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,36 +2,40 @@ import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 class App extends React.Component {
   render() {
     return (
       <div className='calc-wrapper' >
+        <CalculatorDisplay />
         {/* answer line */}
-        <NumberButton bgColor='num-button' text='clear' />
-        <ActionButton className='action-button' text='/' />
-
-
-        <NumberButton bgColor='num-button' text='9' />
-        <NumberButton bgColor='num-button' text='8' />
-        <NumberButton bgColor='num-button' text='7' />
-        <ActionButton className='action-button' text='X' />
-
-
-        <NumberButton bgColor='num-button' text='6' />
-        <NumberButton bgColor='num-button' text='5' />
-        <NumberButton bgColor='num-button' text='4' />
-        <ActionButton className='action-button' text='-' />
-
-
-        <NumberButton bgColor='num-button' text='3' />
-        <NumberButton bgColor='num-button' text='2' />
-        <NumberButton bgColor='num-button' text='1' />
-        <ActionButton className='action-button' text='+' />
-
-
-        <NumberButton bgColor='num-button' text='0' />
-        <ActionButton className='action-button' text='=' />
+        <div className='row'>
+          <ActionButton className='wide-btn num-button' text='clear' />
+          <NumberButton bgColor='action-button small-btn' text={'\u00F7'} />
+        </div>
+        <div className='row'>
+          <NumberButton bgColor='num-button small-btn' text='7' />
+          <NumberButton bgColor='num-button small-btn' text='8' />
+          <NumberButton bgColor='num-button small-btn' text='9' />
+          <NumberButton bgColor='action-button small-btn' text={'\u00D7'} />
+        </div>
+        <div className='row'>
+          <NumberButton bgColor='num-button small-btn' text='4' />
+          <NumberButton bgColor='num-button small-btn' text='5' />
+          <NumberButton bgColor='num-button small-btn' text='6' />
+          <NumberButton bgColor='action-button small-btn' text={'\u2212'} />
+        </div>
+        <div className='row'>
+          <NumberButton bgColor='num-button small-btn' text='1' />
+          <NumberButton bgColor='num-button small-btn' text='2' />
+          <NumberButton bgColor='num-button small-btn' text='3' />
+          <NumberButton bgColor='action-button small-btn' text='+' />
+        </div>
+        <div className='row'>
+          <ActionButton className='wide-btn num-button ' text='0' />
+          <NumberButton bgColor='action-button small-btn' text='=' />
+        </div>
 
       </div >
     )
