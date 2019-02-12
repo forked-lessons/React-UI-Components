37a696bfb3a1c1acd8d921dcdbd239bcc05cbe37
Daniel <danielstarling4@gmail.com>
2019-02-12 13:55:57 -0700

    Calc number buttons created

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className='calc-wrapper' >
+        <NumberButton bgColor='num-button' text='9' />
+        <NumberButton bgColor='num-button' text='8' />
+        <NumberButton bgColor='num-button' text='7' />
+        <NumberButton bgColor='num-button' text='6' />
+        <NumberButton bgColor='num-button' text='5' />
+        <NumberButton bgColor='num-button' text='4' />
+        <NumberButton bgColor='num-button' text='3' />
+        <NumberButton bgColor='num-button' text='2' />
+        <NumberButton bgColor='num-button' text='1' />
+        <NumberButton bgColor='num-button' text='0' />
+      </div >
+    )
+  }
+}
 
 export default App;
